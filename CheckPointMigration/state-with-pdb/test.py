22822cf81a16a6d63fd984fd8108d8dd3aa0eed1
subhrendu1987 <subhrendu@iitg.ernet.in>
2019-08-01 17:31:51 +0530

    modified:   test.py

--- CheckPointMigration/state-with-pdb/test.py
+++ CheckPointMigration/state-with-pdb/test.py
@@ -1,21 +1,12 @@
-# Python program to demonstrate sorting by user's 
-# choice 
-  
-# function to return the second element of the 
-# two elements passed as the paramater 
-def sortSecond(val): 
-    return val[1]  
-  
-# list1 to demonstrate the use of sorting  
-# using using second key  
-list1 = [(1, 2), (3, 3), (1, 1)] 
-  
-# sorts the array in ascending according to  
-# second element 
-list1.sort(key = sortSecond)  
-print(list1) 
-  
-# sorts the array in descending according to 
-# second element 
-list1.sort(key = sortSecond, reverse = True) 
-print(list1) 
+#!/bin/python2.7
+import itertools, time
+from datetime import datetime
+ 
+# Wait for 5 seconds
+i=0
+for elt in itertools.count():
+	now = datetime.now()
+	i=i+1
+	current_time = now.strftime("%H:%M:%S")
+	print("Current Time =", current_time)
+	time.sleep(5)
