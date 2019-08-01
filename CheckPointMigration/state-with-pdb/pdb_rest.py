afebfeb3fded7688ede5c50623b8ed4ab94ca6b8
subhrendu1987 <subhrendu@iitg.ernet.in>
2019-08-01 18:53:38 +0530

    modified:   pdb_rest.py

--- CheckPointMigration/state-with-pdb/pdb_rest.py
+++ CheckPointMigration/state-with-pdb/pdb_rest.py
@@ -1,51 +1,32 @@
 #!/usr/bin/python
-from bottle import Bottle, request
+from bottle import route, run, template
 import time, subprocess, json, sys
 sys.path.append("../modified_pdb")
-import modified_pdb
-from bottledaemon import daemon_run
+#import modified_pdb
+#from bottledaemon import daemon_run
 """
-MininetRest adds a REST API to mininet.
+PdbRest adds a REST API to Pdb.
 """
-protocols='OpenFlow13'
-IP="127.0.0.1"
+IP='localhost'
 PORT="38081"
 ##########################################################################################
-class PdbRest(Bottle):
-    ############################################################
-    def run(self,**kwargs):
-        #daemon_run(**kwargs)
-        super(PdbRest, self).run(**kwargs)
-        #print("Run Called")
-        #This can not be used as mininet is already running in main thread
-        #super(MininetRest, self).run(reloader=True,**kwargs) 
-    ############################################################
-    def __init__(self, net):
-        super(PdbRest, self).__init__()
-        self.prog_name = ""
-        self.route('/start/<prog_name>', callback=self.start)
-        '''
-        self.route('/nodes', callback=self.get_nodes)
-        self.route('/nodes/<node_name>', callback=self.get_node)
-        self.route('/nodes/post/<node_name>/<params>', method='GET', callback=self.post_node)
-        self.route('/nodes/cmd/<cmd_name>', method='GET', callback=self.do_cmd)
-        self.route('/nodes/<node_name>/mnexec/<cmd>', method='GET', callback=self.mnexec)
-        self.route('/nodes/<node_name>/cmdPrint/<cmd>', method='GET', callback=self.cmdPrint)
-        self.route('/switches/rules/<switch_name>', method='GET', callback=self.ovsrules)
-        self.route('/nodes/<node_name>/<intf_name>', callback=self.get_intf)
-        self.route('/nodes/post/<node_name>/<intf_name>/<params>', method='GET', callback=self.post_intf)
-        self.route('/hosts', method='GET', callback=self.get_hosts)
-        self.route('/hosts/post/<host_name>', method='GET', callback=self.get_host_info)
-        self.route('/switches', method='GET', callback=self.get_switches)
-        self.route('/links', method='GET', callback=self.get_links)
-        self.route('/controllers', method='GET', callback=self.get_ctlrs)
-        self.route('/ctlrport', method='GET', callback=self.get_ctlrs_wsport)
-        self.route('/neighbor', method='GET', callback=self.get_neighbors)
-        self.route('/interfaces', method='GET', callback=self.get_intfs)
-        '''
-    ############################################################
-    def start(self, prog_name, params):
-        ''' Start program in pdb mode'''
-        return({'Return':"%s started"%(prog_name)})
-    ############################################################
+############################################################
+@route('/start/')
+@route('/start/<prog_name>')
+def start(prog_name=''):
+	''' Start program in pdb mode
+	http://127.0.0.1:38081/start/test.py'''
+	if(len(prog_name)>0):
+		return({'Return':"%s started"%(prog_name)})
+	else:
+		return template('Goto <a>{{redir_url}}</a>. Missing prog_name', redir_url="http://127.0.0.1:38081/test.py")
+############################################################
+
+
+
 ##########################################################################################
+run(host=IP, port=PORT, debug=True)
+##########################################################################################
+
+
+    
