5f40c53a7a76e992bf7b042c230725c2db8ac60f
subhrendu1987 <subhrendu@iitg.ernet.in>
2019-08-15 08:23:54 +0530

    modified:   input.txt 	modified:   modified_pdb.py 	modified:   modified_pdb_rest.py

--- CheckPointMigration/modified_pdb/modified_pdb_rest.py
+++ CheckPointMigration/modified_pdb/modified_pdb_rest.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python3.7
+#!/usr/bin/python2.7
 from bottle import route, run, template
 import time, subprocess, json, sys
-import modified_pdb as mpdb
-import os
+#import pdb
+#import os
+import modified_pdb as pdb
 #from bottledaemon import daemon_run
 """
 PdbRest adds a REST API to Pdb.
@@ -10,32 +11,50 @@ PdbRest adds a REST API to Pdb.
 IP='localhost'
 PORT="38081"
 ##########################################################################################
-############################################################
+class WrapperPDB(pdb.Pdb):
+	def __init__(self, completekey='tab', stdin=None, stdout=None, skip=None):
+		self.pdbObj=pdb.Pdb.__init__(self,completekey, stdin, stdout, skip)
+		if stdin or stdout:
+			self.use_rawinput = 0
+	############################################################
+	def do_EOF(self, arg):
+		import sys
+		print >>self.stdout
+		#self._user_requested_quit = 1
+		#self.set_quit()
+		self.stdin = sys.stdin
+		return 0
+
+##########################################################################################
+
+wpdb=None
 @route('/start/')
 @route('/start/<prog_name>')
 def start(prog_name=''):
         ''' Start program in pdb mode
         http://127.0.0.1:38081/start/test.py'''
         if(len(prog_name)>0):
+                import os
                 if not os.path.exists(prog_name):
                         print('Error:', prog_name, 'does not exist')
                         sys.exit(1)
                 #sys.stdin = open('input.txt')
-                pdb = mpdb.Pdb(stdin=open('input.txt',"r"))
-                while True:
-                        pdb._runscript(prog_name)
-                        if pdb._user_requested_quit:
+                wpdb = WrapperPDB(stdin=open('input.txt',"r"))
+                wpdb._runscript(prog_name)
+                '''while True:
+                        wpdb._runscript(prog_name)
+                        if wpdb._user_requested_quit:
                                 break
                         else:
                                 print("Execution continued")
+                '''
                 return({'Return':"%s Pdb session exited"%(prog_name)})
         else:
                 return template('Goto <a>{{redir_url}}</a>. Missing prog_name', redir_url="http://127.0.0.1:38081/test.py")
 ############################################################
 @route('/hello/')
 def start():
-	print("Hello")
-
+	return ({"Response":"hello"})
 ##########################################################################################
 run(host=IP, port=PORT, debug=True)
 ##########################################################################################
